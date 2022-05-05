6f644746558180e2808bfcbde69cb73fd379b8bc
Dylan <user@users-MacBook-Pro-2.local>
2022-05-06 00:09:39 +0100

    Assigning variables for highscore.js

---
+++ assets/js/highscores.js
@@ -0,0 +1,5 @@
+const HighScoreTable = document.getElementById("highscores-table");
+const ClearHighscoreBtn = document.getElementById("clear-highscores");
+
+// Listening for click event
+ClearHighscoreBtn.addEventListener("click", clearHighscores);
